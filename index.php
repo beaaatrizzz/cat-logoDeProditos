11cb040ed2147857dff8f769fb147b3b312882b1
Biaa <146894780+beaaatrizzz@users.noreply.github.com>
2024-11-25 09:26:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt=br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estoque</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<h1>Cadastrar Novo Produto</h1>
+
+    <div class="tudo">
+
+
+        <div class="buttons-container">
+            <button><a href="catalogo.php">Ver Catálogo de Produtos</a></button>
+            <button><a href="novoProduto.php">Cadastrar Novo Produto</a></button>
+        </div>
+        
+    </div>
+
+    <footer>
+       <p>Desenvolvido por Beatriz Carvalho</a>
+        <br> Técnica em Desenvolvimento de Sistemas
+    </p> 
+    </footer>
+        
+
+</body>
+</html>
